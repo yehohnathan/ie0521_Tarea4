3f9ca2bad70c56eac3e4dc73be85bb0cca4a1d86
yehohnathan <yehohnathan.miranda@ucr.ac.cr>
2024-06-25 01:32:14 -0600

    Arreglos de flake8
    Signed-off-by: yehohnathan <yehohnathan.miranda@ucr.ac.cr>

--- src/base_parte1/cache_sim.py
+++ src/base_parte1/cache_sim.py
@@ -1,6 +1,6 @@
 from optparse import OptionParser
 import gzip
-from cache import *
+from cache import cache
 
 parser = OptionParser()
 parser.add_option("-s", dest="cache_capacity")
@@ -9,19 +9,21 @@ parser.add_option("-b", dest="block_size")
 parser.add_option("-r", dest="repl_policy")
 parser.add_option("-t", dest="TRACE_FILE")
 
+
 (options, args) = parser.parse_args()
 
-cache = cache(options.cache_capacity, options.cache_assoc, options.block_size, options.repl_policy)
+cache = cache(options.cache_capacity, options.cache_assoc,
+              options.block_size, options.repl_policy)
 
-i = 0 #SOLO PARA DEBUG
-with gzip.open(options.TRACE_FILE,'rt') as trace_fh:
+i = 0   # SOLO PARA DEBUG
+with gzip.open(options.TRACE_FILE, 'rt') as trace_fh:
     for line in trace_fh:
         line = line.rstrip()
-        access_type, hex_str_address  = line.split(" ")
+        access_type, hex_str_address = line.split(" ")
         address = int(hex_str_address, 16)
         cache.access(access_type, address)
-        #SOLO PARA DEBUG
-        #i+=1
-        #if i == 25:
+        # SOLO PARA DEBUG
+        # i+=1
+        # if i == 25:
         #    break
 cache.print_stats()
