da4a737638617e6723e3ca2be273dfa5f50a4d55
yehohnathan <yehohnathan.miranda@ucr.ac.cr>
2024-07-03 18:51:17 -0600

    Ahora se puede seleccionar n pruebas a hacer
    Signed-off-by: yehohnathan <yehohnathan.miranda@ucr.ac.cr>

--- src/main.py
+++ src/main.py
@@ -1,17 +1,53 @@
-""" Importa la clase Automatizador"""
 from Automatizador import Automatizador
+from AutomatizadorMulticache import AutomatizadorMulticache
 
 
 def main():
     automatizadorP1 = Automatizador()
-    # Verifica la tabla:
-    automatizadorP1.table_example()
+    automatizadorMultiple = AutomatizadorMulticache()
 
-    # Realiza todas las pruebas solicitadas
-    automatizadorP1.mod_size_cache()
-    automatizadorP1.mod_assoc_cache()
-    automatizadorP1.mod_size_block()
-    automatizadorP1.mod_repl_policy()
+    # Opciones de pruebas
+    opciones = {
+        1: ("Modificar tamaño de caché",
+            automatizadorP1.mod_size_cache),
+        2: ("Modificar asociatividad del caché",
+            automatizadorP1.mod_assoc_cache),
+        3: ("Modificar tamaño de bloque",
+            automatizadorP1.mod_size_block),
+        4: ("Modificar política de reemplazo",
+            automatizadorP1.mod_repl_policy),
+        5: ("Presencia de caché L1",
+            automatizadorMultiple.presencia_L1),
+        6: ("Presencia de caché L2 (configuración a)",
+            automatizadorMultiple.presencia_L2_a),
+        7: ("Presencia de caché L2 (configuración b)",
+            automatizadorMultiple.presencia_L2_b),
+        8: ("Presencia de caché L3 (configuración a)",
+            automatizadorMultiple.presencia_L3_a),
+        9: ("Presencia de caché L3 (configuración b)",
+            automatizadorMultiple.presencia_L3_b)
+    }
+
+    # Imprimir opciones de prueba
+    print("Seleccione la prueba que desea ejecutar:")
+    for key, (descripcion, _) in opciones.items():
+        print(f"{key}. {descripcion}")
+
+    # Obtener la selección del usuario
+    selecciones = input("Ingrese los números de las pruebas que desea" +
+                        " ejecutar separados por espacios: ")
+    selecciones = selecciones.split()
+
+    for seleccion_str in selecciones:
+        seleccion = int(seleccion_str)
+        if seleccion in opciones:
+            print(f"Ejecutando prueba: {opciones[seleccion][0]}")
+            opciones[seleccion][1]()
+        else:
+            print(f"Selección inválida: {seleccion}. Se omitirá esta",
+                  "selección.")
+
+    print("Todas las pruebas seleccionadas han sido ejecutadas.")
 
 
 """ ======================== Ejecución del main ======================= """
